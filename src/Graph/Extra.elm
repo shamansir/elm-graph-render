2dcc7e4cea038b1d74d33af3712bac8400501fec
Ulric Wilfred <shaman.sir@gmail.com>
2022-06-28 14:00:30 +0200

    add rendering and helpers from `purs-typeclasses`

--- src/Graph/Extra.elm
+++ src/Graph/Extra.elm
@@ -2,6 +2,7 @@ module Graph.Extra exposing (..)
 
 
 import Graph exposing (Graph)
+import Dict
 
 
 filterMap : (n1 -> Maybe n2) -> Graph n1 e -> Graph n2 e
@@ -28,3 +29,18 @@ filter filterFn graph =
     Graph.fromNodesAndEdges
         (nodes |> List.filter (.label >> filterFn))
         edges
+
+
+noParentsNodes : Graph n e -> List Graph.NodeId
+noParentsNodes g =
+    let
+        oneOrAdd =
+            Maybe.map ((+) 1) >> Maybe.withDefault 1 >> Just
+        nodesThatHaveParents =
+            Graph.edges g
+                |> List.foldl (\{ to } dict -> Dict.update to oneOrAdd dict) Dict.empty
+                |> Dict.keys
+    in
+        Graph.nodes g
+            |> List.filter (\n -> not <| List.member n.id nodesThatHaveParents)
+            |> List.map .id
