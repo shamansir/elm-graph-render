d21137ebe19b17b584040dac0c914a2de5440c9c
Ulric Wilfred <shaman.sir@gmail.com>
2022-07-02 19:13:09 +0200

    the draft for radial distribution, p.2

--- src/Graph/Tree/Geometry/Radial.elm
+++ src/Graph/Tree/Geometry/Radial.elm
@@ -31,61 +31,33 @@ addForest itemSize =
         addDimensions : Tree.Forest a -> Tree.Forest (ItemSize, a)
         addDimensions = List.map <| Tree.map <| \a -> ( ItemSize <| itemSize a, a )
 
-        addY : Float -> ( Position, a ) -> ( Position, a )
-        addY y ( pos, a ) = ( { x = pos.x, y = pos.y + y }, a )
-
-        addX : Float -> ( Position, a ) -> ( Position, a )
-        addX x ( pos, a ) = ( { x = pos.x + x, y = pos.y }, a )
-
         distributeForest : Sector -> Tree.Forest ( ItemSize, a ) -> ( Ring, Tree.Forest ( Position, a ) )
         distributeForest sector forest =
-            ( { minRadius = 0, maxRadius = 100 }, [] )
+            let
+                extractRoot tree =
+                    Tree.root tree |> Maybe.map Tuple.first
+                onlyRoots = forest |> List.map extractRoot
 
-
-        {- distributeTree : Position -> Sector -> Tree.Tree ( ItemSize, a ) -> ( Sector, Tree.Tree ( Position, a ) )
-        distributeTree rootPos sector tree =
-            case Tree.root tree of
-                Just ( ( ItemSize rootSize, a ), children ) ->
-
-                    let
-
-                        ( AreaSize childrenArea, shiftedChildren ) =
-                            children
-                                |> List.map distributeTree
-                                |> List.foldl
-                                    (\( AreaSize childArea, t ) ( AreaSize prevArea, items ) ->
-                                        ( AreaSize
-                                            { width = prevArea.width + childArea.width
-                                            , height = max prevArea.height childArea.height
+                rootsCount = onlyRoots |> List.filterMap identity |> List.length
+                perRoot = (sector.maxDegree - sector.minDegree) / toFloat rootsCount
+                distributedBySector =
+                    forest |> List.indexedMap
+                        (\idx tree ->
+                            case Tree.root tree of
+                                Just ( ( _ {- ItemSize rootSize -}, a ), children ) ->
+                                    let
+                                        treeSector =
+                                            { minDegree = toFloat idx * perRoot
+                                            , maxDegree = (toFloat idx + 1) * perRoot
                                             }
-                                        , Tree.map (addX prevArea.width) t :: items
-                                        )
-                                    ) (none, [])
-                                |> Tuple.mapSecond (List.reverse >> List.map (Tree.map <| addY rootSize.height))
-
-                    in
-                        ( AreaSize
-                            { width = max childrenArea.width rootSize.width
-                            , height = rootSize.height + childrenArea.height
-                            }
-                        , Tree.inner ( rootPos, a )
-                            <| if childrenArea.width >= rootSize.width
-                                then shiftedChildren
-                                else shiftedChildren |> moveToCenter
+                                    in
+                                        Tree.inner ( { x = 0, y = 0 }, a )
+                                            <| Tuple.second
+                                            <| distributeForest treeSector children
+                                Nothing -> Tree.empty
                         )
-
-                Nothing ->
-                    ( none, Tree.empty )
-
-        distributeTreesOverRadius : List ( AreaSize, Tree.Tree ( Position, a ) ) -> Geometry a
-        distributeTreesOverRadius =
-            List.foldl
-                (\( AreaSize curArea, tree ) ( AreaSize prevArea, list ) ->
-                    ( AreaSize { width = prevArea.width + curArea.width, height = max curArea.height prevArea.height }
-                    , (tree |> Tree.map (addX prevArea.width)) :: list
-                    )
-                )
-                ( none, [] ) -}
+            in
+                ( { minRadius = 0, maxRadius = 100 }, distributedBySector )
 
 
         findAreaSize : ( Ring, Tree.Forest ( Position, a ) ) -> Geometry a
